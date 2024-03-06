ba94eb77ac321b5bcf1280708f3abe3024bad3dc
Baalais <99286765+baalais@users.noreply.github.com>
2024-03-06 11:20:52 +0200

    menu
    menu

--- coffe_shop/src/components/FirstPage.jsx
+++ coffe_shop/src/components/FirstPage.jsx
@@ -6,26 +6,27 @@ import coffeBean from '../assets/icons/coffeeBean.png';
 import coffePot from '../assets/icons/coffeePot.png';
 
 function Firstpage() {
-	return (
-		<div id='home' className={css.firstpage}>
-			<img className={`icon ${css.coffe}`} src={coffe} alt='Coffee Icon' />
-			<img className={`icon ${css.coffePot}`} src={coffePot} alt='Coffee Pot Icon' />
-			<img className={`icon ${css.coffeBean}`} src={coffeBean} alt='Coffee Bean Icon' />
-			<img className={`icon ${css.coffeBean2}`} src={coffeBean} alt='Coffee Bean Icon' />
+    return (
+        <div id='home' className={css.firstpage}>
+            <img className={`icon ${css.coffe}`} src={coffe} alt='Coffee Icon' />
+            <img className={`icon ${css.coffePot}`} src={coffePot} alt='Coffee Pot Icon' />
+            <img className={`icon ${css.coffeBean}`} src={coffeBean} alt='Coffee Bean Icon' />
+            <img className={`icon ${css.coffeBean2}`} src={coffeBean} alt='Coffee Bean Icon' />
 
-			<h1 className={css.bigText}>Coffe Time</h1>
-			<img className={css.background775} src={background775} />
-			<div className={css.contents}>
-				<h2 className={css.h2}>Your headline</h2>
-				<p className={css.p}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore
-				</p>
-				<button className={css.orderButton}>Order Now</button>
-			</div>
-			<img className={css.wave1} src={wave1} />
-		</div>
-	);
+            <h1 className={css.bigText}>Coffe Time</h1>
+            <img className={css.background775} src={background775} />
+            <div className={css.contents}>
+                <h2 className={css.h2}>Your headline</h2>
+                <p className={css.p}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore
+                </p>
+                <button className={css.orderButton}>Order Now</button>
+            </div>
+            <img className={css.wave1} src={wave1} />
+        </div>
+    );
 }
 
 export default Firstpage;
+
--- coffe_shop/src/components/Navbar.jsx
+++ coffe_shop/src/components/Navbar.jsx
@@ -7,71 +7,71 @@ import '../style/Hamburger.css';
 function Navbar() {
     // Use state to manage the visibility of the hamburger menu
     const [isHamburgerVisible, setHamburgerVisible] = useState(false);
-  
+
     // Function to toggle the visibility of the hamburger menu
     function toggleHamburger() {
-      setHamburgerVisible(!isHamburgerVisible);
-    }
-  
-    // Function to handle button clicks inside the hamburger menu
-    function handleHamburgerButtonClick() {
-      setHamburgerVisible(false);
+        setHamburgerVisible(!isHamburgerVisible);
     }
 
-  return (
-    <>
-      <div className={css.header}>
-        <div className={`${css.navBar} ${isHamburgerVisible ? css.hide : ''}`}>
-          <a href='#home' className={css.navbarItem}>
-            <h2 className={css.navbarText}>Home</h2>
-          </a>
-          <a href='#about' className={css.navbarItem}>
-                <h2 className={css.navbarText}>About</h2>
-          </a>
-          <a href='#menu' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Menu</h2>
-          </a>
-          <a href='#blog' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Blog</h2>
-          </a>
-          <a href='#shop' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Shop</h2>
-          </a>
-          <a href='#contact' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Contact</h2>
-          </a>
-        </div>
-        <span onClick={toggleHamburger} id='hamburgerIcon' className='material-symbols-outlined'>
-          menu
-        </span>
-        <div id='hamburger' className={`${css.hamburger} ${isHamburgerVisible ? '' : css.hide}`}>
-          <a href='#home' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
-            <h2 className={css.navbarText}>Home</h2>
-          </a>
-          <a href='#about' id='navItem' className={css.navbarItem}>
-                <h2 className={css.navbarText}>About</h2>
-        </a>
-        <a href='#menu' id='navItem' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Menu</h2>
-        </a>
-        <a href='#blog' id='navItem' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Blog</h2>
-        </a>
-        <a href='#shop' id='navItem' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Shop</h2>
-        </a>
-        <a href='#contact' id='navItem' className={css.navbarItem}>
-                <h2 className={css.navbarText}>Contact</h2>
-        </a>
-        </div>
-        <div className={css.logoBar}>
-          <Link to='/' className={css.logoItem}>
-            <img src={logo} alt="Logo" />
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+    // Function to handle button clicks inside the hamburger menu
+    function handleHamburgerButtonClick() {
+        setHamburgerVisible(false);
+    }
+
+    return (
+        <>
+            <div className={css.header}>
+                <div className={`${css.navBar} ${isHamburgerVisible ? css.show : ''}`}>
+                    <a href='#home' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>Home</h2>
+                    </a>
+                    <a href='#about' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>About</h2>
+                    </a>
+                    <a href='#menu' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>Menu</h2>
+                    </a>
+                    <a href='#blog' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>Blog</h2>
+                    </a>
+                    <a href='#shop' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>Shop</h2>
+                    </a>
+                    <a href='#contact' className={css.navbarItem}>
+                        <h2 className={css.navbarText}>Contact</h2>
+                    </a>
+                </div>
+                <span onClick={toggleHamburger} id='hamburgerIcon' className='material-symbols-outlined'>
+                    menu
+                </span>
+                <div id='hamburger' className={`${css.hamburger} ${isHamburgerVisible ? '' : css.show}`}>
+                    <a href='#home' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>Home</h2>
+                    </a>
+                    <a href='#about' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>About</h2>
+                    </a>
+                    <a href='#menu' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>Menu</h2>
+                    </a>
+                    <a href='#blog' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>Blog</h2>
+                    </a>
+                    <a href='#shop' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>Shop</h2>
+                    </a>
+                    <a href='#contact' id='navItem' className={css.navbarItem} onClick={handleHamburgerButtonClick}>
+                        <h2 className={css.navbarText}>Contact</h2>
+                    </a>
+                </div>
+                <div className={css.logoBar}>
+                    <Link to='/' className={css.logoItem}>
+                        <img src={logo} alt="Logo" />
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
